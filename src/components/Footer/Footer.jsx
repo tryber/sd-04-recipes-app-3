d85e874533f7c473db856fa9e61331050b3c384c
danilossousa <danilosantossousa@gmail.com>
2020-07-31 22:00:44 -0300

    menu inferior t2

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,20 +5,17 @@ import exploreIcon from '../../images/exploreIcon.svg';
 import mealIcon from '../../images/mealIcon.svg';
 import './Footer.css';
 
-const Footer = () => {
-  return (
-    <footer data-testid="footer">
-      <Link to="/bebidas">
-        <img src={drinkIcon} alt="Ícone de bebida" />
-      </Link>
-      <Link to="/explorar">
-        <img src={exploreIcon} alt="Ícone de exploração" />
-      </Link>
-      <Link to="/comidas">
-        <img data-testid="food-bottom-btn" src={mealIcon} alt="Ícone de refeição" />
-      </Link>
-    </footer>
-  );
-};
+const Footer = () =>
+  <footer data-testid="footer">
+    <Link to="/bebidas">
+      <img src={drinkIcon} alt="Ícone de bebida" />
+    </Link>
+    <Link to="/explorar">
+      <img src={exploreIcon} alt="Ícone de exploração" />
+    </Link>
+    <Link to="/comidas">
+      <img data-testid="food-bottom-btn" src={mealIcon} alt="Ícone de refeição" />
+    </Link>
+  </footer>;
 
 export default Footer;
