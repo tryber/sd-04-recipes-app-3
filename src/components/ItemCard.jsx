dfb54f139eb56584100e26bb0fd02128d41ea64e
djonata-dias <diasdjonata@gmail.com>
2020-08-05 13:00:20 -0300

    criei o componente itemCard que retorna uma div estilizada em formato de card com as informações passadas por props

--- src/components/ItemCard.jsx
+++ src/components/ItemCard.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const ItemCard = () =>
-  // o data-testid devera ser modificado ao utilizar os dados da API
-  <div className="item-card" data-testid="${index}-recipe-card">
-    <Link>
+const ItemCard = (name, image, index, link) =>
+  <div className="item-card card text-center" data-testid={`${index}-recipe-card`}>
+    <Link to={link}>
       <div>
-        <img src="" alt="Imagem da receita" />
-        <h1>Title</h1>
+        <img src={image} alt="Imagem da receita" className="card-img-top" />
+        <h1 className="card-title badge badge-secondary">{name}</h1>
       </div>
     </Link>
   </div>;
