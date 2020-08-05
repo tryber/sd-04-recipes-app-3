194a3d01f0d1fa5ff9dae4fb61420ca69f455f82
djonata-dias <diasdjonata@gmail.com>
2020-08-05 18:18:27 -0300

    importei o css na pagina de login

--- src/pages/LoginScreen.js
+++ src/pages/LoginScreen.js
@@ -1,48 +1,62 @@
 import React from 'react';
+import '../css/loginScreen.css';
 
 function validateEmail(email) {
   const re = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+(\.[a-z]+)?$/i;
   return re.test(email);
 }
 const LoginScreen = () => (
-  <form
-    onSubmit={(e) => {
-      e.preventDefault();
-      const emailValue = document.getElementById('email-input').value;
-      // const passValue = document.getElementById('password-input');
-      if (!validateEmail(emailValue)) {
-        alert('Digite um email valido!');
-        return false;
-      }
-      return true;
-    }}
-    onChange={() => {
-      const emailValue = document.getElementById('email-input').value;
-      const passValue = document.getElementById('password-input').value;
-      const submitButton = document.getElementById('login-submit-btn');
-      console.log(emailValue);
-      if (passValue && validateEmail(emailValue)) {
-        submitButton.disabled = false;
-      } else {
-        submitButton.disabled = true;
-      }
-    }}
-  >
-    <input
-      id="email-input"
-      data-testid="email-input"
-      type="email"
-      placeholder="Email"
-    />
-    <input
-      id="password-input"
-      data-testid="password-input"
-      minLength="6"
-      type="password"
-      placeholder="Password"
-    />
-    <button type="button" id="login-submit-btn" data-testid="login-submit-btn" disabled>Login</button>
-  </form>
+  <div className="login-container">
+    <form
+      onSubmit={(e) => {
+        e.preventDefault();
+        const emailValue = document.getElementById('email-input').value;
+        // const passValue = document.getElementById('password-input');
+        if (!validateEmail(emailValue)) {
+          alert('Digite um email valido!');
+          return false;
+        }
+        return true;
+      }}
+      onChange={() => {
+        const emailValue = document.getElementById('email-input').value;
+        const passValue = document.getElementById('password-input').value;
+        const submitButton = document.getElementById('login-submit-btn');
+        console.log(emailValue);
+        if (passValue && validateEmail(emailValue)) {
+          submitButton.disabled = false;
+        } else {
+          submitButton.disabled = true;
+        }
+      }}
+      className="form-group"
+    >
+      <input
+        id="email-input"
+        data-testid="email-input"
+        type="email"
+        placeholder="Email"
+        className="form-control"
+      />
+      <input
+        id="password-input"
+        data-testid="password-input"
+        minLength="6"
+        type="password"
+        placeholder="Password"
+        className="form-control"
+      />
+      <button
+        type="submit"
+        id="login-submit-btn"
+        data-testid="login-submit-btn"
+        className="btn btn-primary"
+        disabled
+      >
+        Login
+    </button>
+    </form>
+  </div>
 );
 
 export default LoginScreen;
