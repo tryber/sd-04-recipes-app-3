22b54b7051f539205333004f7b98c384e677580c
danilossousa <danilosantossousa@gmail.com>
2020-08-06 15:31:35 -0300

    componente de recomendaçoes t5

--- src/pages/CocktailDetailScreen.js
+++ src/pages/CocktailDetailScreen.js
@@ -3,6 +3,18 @@ import { useDispatch, useSelector } from 'react-redux';
 import Recommendations from '../components/Recommendations/Recommendations';
 import { fetchRec } from '../actions/recRequest';
 
+// Função q separa as 6 primeiras recomendações caso os dados já tenham
+// sido armazenados na store
+const getSixRecs = (meals, sixRecs) => {
+  if (meals !== undefined) {
+    for (let i = 0; i < meals.length; i += 1) {
+      if (i > 5) break;
+      sixRecs.push(meals[i]);
+    }
+  }
+  return sixRecs;
+}
+
 const CocktailDetailScreen = () => {
   const dispatch = useDispatch();
   const loading = useSelector((state) => state.recommendations.loading);
@@ -13,17 +25,10 @@ const CocktailDetailScreen = () => {
     dispatch(fetchRec('https://www.themealdb.com/api/json/v1/1/search.php?s='));
   }, [dispatch]);
 
-  if (meals !== undefined) {
-    for (let i = 0; i < meals.length; i += 1) {
-      if (i > 5) break;
-      sixRecs.push(meals[i]);
-    }
-  }
-
   return (
     <div>
       <h1>Detalhes - Bebida</h1>
-      {loading === null ? 'Loading...' : <Recommendations sixRecs={sixRecs} />}
+      {loading === null ? 'Loading...' : <Recommendations sixRecs={getSixRecs(meals, sixRecs)} />}
     </div>
   );
 };
--- src/pages/MealDetailScreen.js
+++ src/pages/MealDetailScreen.js
@@ -3,6 +3,18 @@ import { useDispatch, useSelector } from 'react-redux';
 import Recommendations from '../components/Recommendations/Recommendations';
 import { fetchRec } from '../actions/recRequest';
 
+// Função q separa as 6 primeiras recomendações caso os dados já tenham
+// sido armazenados na store
+const getSixRecs = (drinks, sixRecs) => {
+  if (drinks !== undefined) {
+    for (let i = 0; i < drinks.length; i += 1) {
+      if (i > 5) break;
+      sixRecs.push(drinks[i]);
+    }
+  }
+  return sixRecs;
+}
+
 const MealDetailScreen = () => {
   const dispatch = useDispatch();
   const loading = useSelector((state) => state.recommendations.loading);
@@ -13,17 +25,10 @@ const MealDetailScreen = () => {
     dispatch(fetchRec('https://www.thecocktaildb.com/api/json/v1/1/search.php?s='));
   }, [dispatch]);
 
-  if (drinks !== undefined) {
-    for (let i = 0; i < drinks.length; i += 1) {
-      if (i > 5) break;
-      sixRecs.push(drinks[i]);
-    }
-  }
-
   return (
     <div>
       <h1>Detalhes - Comida</h1>
-      {loading === null ? 'Loading...' : <Recommendations sixRecs={sixRecs} />}
+      {loading === null ? 'Loading...' : <Recommendations sixRecs={getSixRecs(drinks, sixRecs)} />}
     </div>
   );
 };
