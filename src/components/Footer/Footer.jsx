c1f87259a6e9555b7959e24e9ee888164013eee3
djonata-dias <diasdjonata@gmail.com>
2020-08-06 16:57:03 -0300

    troquei o caminho do css devido a centralizaçao dos arquivos na mesma pasta

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 import drinkIcon from '../../images/drinkIcon.svg';
 import exploreIcon from '../../images/exploreIcon.svg';
 import mealIcon from '../../images/mealIcon.svg';
-import './Footer.css';
+import '../../css/Footer.css';
 
 const Footer = () =>
   <footer data-testid="footer">
