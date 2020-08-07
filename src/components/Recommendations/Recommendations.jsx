bae65fa8ec10d2d036a7dddf592c0e723cc9a5c3
djonata-dias <diasdjonata@gmail.com>
2020-08-07 15:18:48 -0300

    troquei o caminho do css

--- src/components/Recommendations/Recommendations.jsx
+++ src/components/Recommendations/Recommendations.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useLocation } from 'react-router-dom';
 import PropTypes from 'prop-types';
-import './Recommendations.css';
+import '../../css/Recommendations.css';
 
 const Recommendations = ({ sixRecs }) => {
   const location = useLocation();
