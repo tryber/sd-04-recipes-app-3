5826c8b28eef0bc4370f921e7ce8c6b9301054ab
djonata-dias <diasdjonata@gmail.com>
2020-08-09 12:10:17 -0300

    ajustei o carrossel

--- src/components/Recommendations/Recommendations.jsx
+++ src/components/Recommendations/Recommendations.jsx
@@ -5,27 +5,24 @@ import '../../css/Recommendations.css';
 
 const Recommendations = ({ sixRecs }) => {
   const location = useLocation();
-
   return (
     <div>
       <h1>Recomendadas</h1>
       <div className="carousel">
         <div className="content">
           {location.pathname.startsWith('/comidas')
-
-            ? sixRecs.map((rec, index) =>
+            ? (sixRecs.map((rec, index) =>
               <div className="rec" key={rec.idDrink} data-testid={`${index}-recomendation-card`}>
-                <img src={rec.strDrinkThumb} alt="Recomendações" />
-                <p>{rec.strAlcoholic}</p>
-                <p data-testid={`${index}-recomendation-title`} className="title">{rec.strDrink}</p>
-              </div>)
-
-            : sixRecs.map((rec, index) =>
-              <div className="rec" key={rec.idMeal} data-testid={`${index}-recomendation-card`}>
                 <img src={rec.strMealThumb} alt="Recomendações" />
                 <p>{rec.strCategory}</p>
+                <p data-testid={`${index}-recomendation-title`} className="title">{rec.strDrink}</p>
+              </div>))
+            : (sixRecs.map((rec, index) =>
+              <div className="rec" key={rec.idMeal} data-testid={`${index}-recomendation-card`}>
+                <img src={rec.strDrinkThumb} alt="Recomendações" />
+                <p>{rec.strCategory}</p>
                 <p data-testid={`${index}-recomendation-title`} className="title">{rec.strMeal}</p>
-              </div>)
+              </div>))
           }
         </div>
       </div>
