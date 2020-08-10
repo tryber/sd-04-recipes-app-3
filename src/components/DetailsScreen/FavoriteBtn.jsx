cf5f24bba9823ea3387db1d2dec4d43005c181f1
Pedro <pedrotpoliveira@gmail.com>
2020-08-10 19:23:11 -0300

    SetFavorites

--- src/components/DetailsScreen/FavoriteBtn.jsx
+++ src/components/DetailsScreen/FavoriteBtn.jsx
@@ -5,28 +5,33 @@ import favoriteIconWhite from '../../images/whiteHeartIcon.svg';
 import favoriteIconBlack from '../../images/blackHeartIcon.svg';
 import { getRouteInfo } from '../../helpers';
 
-const createFavoriteItem = (data, location) => {
-  const isFood = getRouteInfo(location).mainRoute === 'comidas';
-  const id = getRouteInfo(location).recipeId;
-  return {
-    id,
-    type: isFood ? 'comida' : 'bebida',
-    area: data.strArea ? data.StrArea : '',
-    category: data.strCategory,
-    alcoholicOrNot: data.strAlcoholic ? data.strAlcoholic : '',
-    name: isFood ? data.strMeal : data.strDrink,
-    image: isFood ? data.strMealThumb : data.strDrinkThumb,
-  };
+const createFavoriteItem = (data, id, isFood) => ({
+  id,
+  type: isFood ? 'comida' : 'bebida',
+  area: data.strArea ? data.StrArea : '',
+  category: data.strCategory,
+  alcoholicOrNot: data.strAlcoholic ? data.strAlcoholic : '',
+  name: isFood ? data.strMeal : data.strDrink,
+  image: isFood ? data.strMealThumb : data.strDrinkThumb,
+});
+
+const checkFavorite = (id) => {
+  const currentFavorites = localStorage.getItem('favoriteRecipes');
+  return currentFavorites.some((item) => item.id === id);
 };
 
-const setLocalFavorite = (favoriteItem) => {
+const setLocalFavorite = (favoriteItem, id) => {
   let currentFavorites = localStorage.getItem('favoriteRecipes');
   if (!currentFavorites) {
     const favorites = JSON.stringify([favoriteItem]);
     localStorage.setItem('favoriteRecipes', favorites);
   } else {
     currentFavorites = JSON.parse(currentFavorites);
-    currentFavorites.push(favoriteItem);
+    if (checkFavorite(id)) {
+      currentFavorites = currentFavorites.filter((item) => item.id !== id);
+    } else {
+      currentFavorites.push(favoriteItem);
+    }
     localStorage.setItem('favoriteRecipes', JSON.stringify(currentFavorites));
   }
 };
@@ -35,7 +40,10 @@ const FavoriteBtn = () => {
   const [favorite, setFavorite] = useState(false);
   const location = useLocation();
   const recipeData = useSelector((state) => state.api.data);
-  const favoriteItem = createFavoriteItem(recipeData[0], location);
+  const id = getRouteInfo(location).recipeId;
+  const isFood = getRouteInfo(location).mainRoute === 'comidas';
+  const favoriteItem = createFavoriteItem(recipeData[0], id, isFood);
+  setFavorite(checkFavorite(id));
 
   return (
     <input
