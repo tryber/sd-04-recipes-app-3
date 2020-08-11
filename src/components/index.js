fcd4556b9b17db9ab769a3150e1554696f5a130c
DanielLMiranda <danieldelacerdamiranda@gmail.com>
2020-08-11 15:38:57 -0300

    fix/cypress error - commented old details screen

--- src/components/index.js
+++ src/components/index.js
@@ -3,6 +3,7 @@ export { default as EmbeddedVideo } from './DetailsScreen/EmbeddedVideo';
 export { default as IngredientsList } from './DetailsScreen/IngredientsList';
 export { default as FavoriteBtn } from './DetailsScreen/FavoriteBtn';
 export { default as ShareBtn } from './DetailsScreen/ShareBtn';
+export { default as Recommendations } from './DetailsScreen/Recommendations';
 
 // Footer
 export { default as Footer } from './Footer/Footer';
@@ -13,9 +14,6 @@ export { default as Header } from './Header/Header';
 // Main Screen
 export { default as CategotyFilter } from './MainScreen/CategotyFilter';
 export { default as ItemCard } from './MainScreen/ItemCard';
-
-// Recommendations
-export { default as Recommendations } from './Recommendations/Recommendations';
 
 // Search Bar
 export { default as SearchBar } from './SearchBar/SearchBar';
--- src/pages/MealDetailScreen.js
+++ src/pages/MealDetailScreen.js
@@ -1,35 +1,35 @@
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import Recommendations from '../components/Recommendations/Recommendations';
-import { fetchRec } from '../actions/recRequest';
+// import React, { useEffect } from 'react';
+// import { useDispatch, useSelector } from 'react-redux';
+// import Recommendations from '../components/Recommendations/Recommendations';
+// import { fetchRec } from '../actions/recRequest';
 
-// Função q separa as 6 primeiras recomendações caso os dados da requisição
-// inicial já tenham sido armazenados na store
-const getSixRecs = (drinks, sixRecs) => {
-  for (let i = 0; i < drinks.length; i += 1) {
-    if (i > 5) break;
-    sixRecs.push(drinks[i]);
-  }
-};
+// // Função q separa as 6 primeiras recomendações caso os dados da requisição
+// // inicial já tenham sido armazenados na store
+// const getSixRecs = (drinks, sixRecs) => {
+//   for (let i = 0; i < drinks.length; i += 1) {
+//     if (i > 5) break;
+//     sixRecs.push(drinks[i]);
+//   }
+// };
 
-const MealDetailScreen = () => {
-  const dispatch = useDispatch();
-  const loading = useSelector((state) => state.recommendations.loading);
-  const drinks = useSelector((state) => state.recommendations.data.drinks);
-  const sixRecs = [];
+// const MealDetailScreen = () => {
+//   const dispatch = useDispatch();
+//   const loading = useSelector((state) => state.recommendations.loading);
+//   const drinks = useSelector((state) => state.recommendations.data.drinks);
+//   const sixRecs = [];
 
-  useEffect(() => {
-    dispatch(fetchRec('https://www.thecocktaildb.com/api/json/v1/1/search.php?s='));
-  }, [dispatch]);
+//   useEffect(() => {
+//     dispatch(fetchRec('https://www.thecocktaildb.com/api/json/v1/1/search.php?s='));
+//   }, [dispatch]);
 
-  if (drinks !== undefined) getSixRecs(drinks, sixRecs);
+//   if (drinks !== undefined) getSixRecs(drinks, sixRecs);
 
-  return (
-    <div>
-      <h1>Detalhes - Comida</h1>
-      {loading === null ? 'Loading...' : <Recommendations sixRecs={sixRecs} />}
-    </div>
-  );
-};
+//   return (
+//     <div>
+//       <h1>Detalhes - Comida</h1>
+//       {loading === null ? 'Loading...' : <Recommendations sixRecs={sixRecs} />}
+//     </div>
+//   );
+// };
 
-export default MealDetailScreen;
+// export default MealDetailScreen;
