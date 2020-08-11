d16f8e9e843ec69eeebf4bc5cd9ed20d92518796
DanielLMiranda <danieldelacerdamiranda@gmail.com>
2020-08-11 15:29:53 -0300

    fix/codeclimate

--- src/components/DetailsScreen/Recommendations.jsx
+++ src/components/DetailsScreen/Recommendations.jsx
@@ -11,19 +11,38 @@ const Recommendations = ({ sixRecs }) => {
       <div className="carousel">
         <div className="content">
           {location.pathname.startsWith('/comidas')
-            ? (sixRecs.map((rec, index) =>
-              <div className="rec" key={rec.idDrink + 1} data-testid={`${index}-recomendation-card`}>
-                <img src={rec.strMealThumb} alt="Recomendações" />
-                <p>{rec.strCategory}</p>
-                <p data-testid={`${index}-recomendation-title`} className="title">{rec.strDrink}</p>
-              </div>))
-            : (sixRecs.map((rec, index) =>
-              <div className="rec" key={rec.idMeal + 1} data-testid={`${index}-recomendation-card`}>
-                <img src={rec.strDrinkThumb} alt="Recomendações" />
-                <p>{rec.strCategory}</p>
-                <p data-testid={`${index}-recomendation-title`} className="title">{rec.strMeal}</p>
-              </div>))
-          }
+            ? sixRecs.map((rec, index) => (
+                <div
+                  className="rec"
+                  key={rec.idDrink + 1}
+                  data-testid={`${index}-recomendation-card`}
+                >
+                  <img src={rec.strMealThumb} alt="Recomendações" />
+                  <p>{rec.strCategory}</p>
+                  <p
+                    data-testid={`${index}-recomendation-title`}
+                    className="title"
+                  >
+                    {rec.strDrink}
+                  </p>
+                </div>
+              ))
+            : sixRecs.map((rec, index) => (
+                <div
+                  className="rec"
+                  key={rec.idMeal + 1}
+                  data-testid={`${index}-recomendation-card`}
+                >
+                  <img src={rec.strDrinkThumb} alt="Recomendações" />
+                  <p>{rec.strCategory}</p>
+                  <p
+                    data-testid={`${index}-recomendation-title`}
+                    className="title"
+                  >
+                    {rec.strMeal}
+                  </p>
+                </div>
+              ))}
         </div>
       </div>
     </div>
