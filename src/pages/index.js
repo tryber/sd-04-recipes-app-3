fc95d6bf73f6fa04cedd11f1d320d521dd85ad23
djonata-dias <diasdjonata@gmail.com>
2020-08-12 14:54:58 -0300

    adicionei a ExploreScreen na exportação de páginas

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,3 +4,4 @@ export { default as LoginScreen } from './LoginScreen';
 export { default as MealDetailScreen } from './MealDetailScreen';
 export { default as MealsScreen } from './MealsScreen';
 export { default as ProfileScreen } from './ProfileScreen';
+export { default as ExploreScreen } from './Explore/ExploreScreen';
