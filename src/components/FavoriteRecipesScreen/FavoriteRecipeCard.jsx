5dc9423ffa5325ec1fdccd96f2683e337da16ad6
DanielLMiranda <danieldelacerdamiranda@gmail.com>
2020-08-12 21:35:22 -0300

    fix/codeclimate

--- src/components/FavoriteRecipesScreen/FavoriteRecipeCard.jsx
+++ src/components/FavoriteRecipesScreen/FavoriteRecipeCard.jsx
@@ -4,39 +4,37 @@ import { Link } from 'react-router-dom';
 import ShareBtn from './ShareBtn';
 import FavoriteBtn from './FavoriteBtn';
 
-const FavoriteRecipeCard = ({ favoriteItem, favoriteItemIndex }) => {
-  return (
-    <div>
-      <Link to={`/${favoriteItem.type}s/${favoriteItem.id}`}>
-        <img
-          src={favoriteItem.image}
-          alt="Recipe food or drink"
-          data-testid={`${favoriteItemIndex}-horizontal-image`}
-        />
-      </Link>
-      <Link to={`/${favoriteItem.type}s/${favoriteItem.id}`}>
-        <h2 data-testid={`${favoriteItemIndex}-horizontal-name`}>
-          {favoriteItem.name}
-        </h2>
-      </Link>
-      {favoriteItem.type === 'comida' ? (
-        <h3 data-testid={`${favoriteItemIndex}-horizontal-top-text`}>
-          {`${favoriteItem.area} - ${favoriteItem.category}`}
-        </h3>
-      ) : (
-        <h3 data-testid={`${favoriteItemIndex}-horizontal-top-text`}>
-          {favoriteItem.alcoholicOrNot}
-        </h3>
-      )}
-      <ShareBtn
-        type={favoriteItem.type}
-        id={favoriteItem.id}
-        index={favoriteItemIndex}
+const FavoriteRecipeCard = ({ favoriteItem, favoriteItemIndex }) => (
+  <div>
+    <Link to={`/${favoriteItem.type}s/${favoriteItem.id}`}>
+      <img
+        src={favoriteItem.image}
+        alt="Recipe food or drink"
+        data-testid={`${favoriteItemIndex}-horizontal-image`}
       />
-      <FavoriteBtn id={favoriteItem.id} index={favoriteItemIndex} />
-    </div>
-  );
-};
+    </Link>
+    <Link to={`/${favoriteItem.type}s/${favoriteItem.id}`}>
+      <h2 data-testid={`${favoriteItemIndex}-horizontal-name`}>
+        {favoriteItem.name}
+      </h2>
+    </Link>
+    {favoriteItem.type === 'comida' ? (
+      <h3 data-testid={`${favoriteItemIndex}-horizontal-top-text`}>
+        {`${favoriteItem.area} - ${favoriteItem.category}`}
+      </h3>
+    ) : (
+      <h3 data-testid={`${favoriteItemIndex}-horizontal-top-text`}>
+        {favoriteItem.alcoholicOrNot}
+      </h3>
+    )}
+    <ShareBtn
+      type={favoriteItem.type}
+      id={favoriteItem.id}
+      index={favoriteItemIndex}
+    />
+    <FavoriteBtn id={favoriteItem.id} index={favoriteItemIndex} />
+  </div>
+);
 
 FavoriteRecipeCard.propTypes = {
   favoriteItem: PropTypes.objectOf(PropTypes.string).isRequired,
