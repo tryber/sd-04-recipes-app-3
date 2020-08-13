62ca4a6f44de1a4cf48322d209d725330bd04e85
DanielLMiranda <danieldelacerdamiranda@gmail.com>
2020-08-13 14:01:55 -0300

    added filter feature

--- src/pages/FavoriteRecipesScreen.js
+++ src/pages/FavoriteRecipesScreen.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import Header from '../components/Header/Header';
 import FavoriteRecipeCard from '../components/FavoriteRecipesScreen/FavoriteRecipeCard';
@@ -8,6 +8,7 @@ import { getLS } from '../helpers';
 const FavoriteRecipesScreen = () => {
   const dispatch = useDispatch();
   const favorites = useSelector((state) => state.favorites);
+  const [filter, setFilter] = useState('');
   useEffect(() => {
     dispatch(favoriteRecipes(getLS('favoriteRecipes')));
   }, []); // eslint-disable-line
@@ -15,23 +16,47 @@ const FavoriteRecipesScreen = () => {
   return (
     <div>
       <Header />
-      <button type="button" data-testid="filter-by-food-btn">
+      <button
+        type="button"
+        data-testid="filter-by-food-btn"
+        onClick={() => setFilter('food')}
+      >
         Food
       </button>
-      <button type="button" data-testid="filter-by-drink-btn">
+      <button
+        type="button"
+        data-testid="filter-by-drink-btn"
+        onClick={() => setFilter('drink')}
+      >
         Drinks
       </button>
-      <button type="button" data-testid="filter-by-all-btn">
+      <button
+        type="button"
+        data-testid="filter-by-all-btn"
+        onClick={() => setFilter('all')}
+      >
         All
       </button>
 
-      {favorites.map((favoriteItem, index) => (
-        <FavoriteRecipeCard
-          key={favoriteItem.id}
-          favoriteItem={favoriteItem}
-          favoriteItemIndex={index}
-        />
-      ))}
+      {favorites
+        .filter((item) => {
+          if (filter === 'food') {
+            return item.type === 'comida';
+          }
+
+          if (filter === 'drink') {
+            return item.type === 'bebida';
+          }
+
+          return item;
+        })
+        .map((favoriteItem, index) => (
+          <FavoriteRecipeCard
+            key={favoriteItem.id}
+            favoriteItem={favoriteItem}
+            favoriteItemIndex={index}
+          />
+        ))}
     </div>
   );
 };
