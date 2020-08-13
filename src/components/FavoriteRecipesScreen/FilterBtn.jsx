8628e5c406f69f371e535c39917c8ae41663c9d5
DanielLMiranda <danieldelacerdamiranda@gmail.com>
2020-08-13 20:20:47 -0300

    fix/codeclimate and added propTypes

--- src/components/FavoriteRecipesScreen/FilterBtn.jsx
+++ src/components/FavoriteRecipesScreen/FilterBtn.jsx
@@ -1,15 +1,21 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const FilterBtn = ({ dataTestId, filter, setFilter, text }) => {
-  return (
-    <button
-      type="button"
-      data-testid={dataTestId}
-      onClick={() => setFilter(filter)}
-    >
-      {text}
-    </button>
-  );
+const FilterBtn = ({ dataTestId, filter, setFilter, text }) => (
+  <button
+    type="button"
+    data-testid={dataTestId}
+    onClick={() => setFilter(filter)}
+  >
+    {text}
+  </button>
+);
+
+FilterBtn.propTypes = {
+  dataTestId: PropTypes.string.isRequired,
+  filter: PropTypes.string.isRequired,
+  setFilter: PropTypes.func.isRequired,
+  text: PropTypes.string.isRequired,
 };
 
 export default FilterBtn;
