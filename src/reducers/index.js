126d75986de508c184c2fe016222c9935d29168d
DanielLMiranda <danieldelacerdamiranda@gmail.com>
2020-08-12 21:29:21 -0300

    created redux structure

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,12 @@
 import { combineReducers } from 'redux';
 import apiRequest from './apiRequest';
 import recRequest from './recRequest';
+import favoriteRecipes from './favoriteRecipes';
 
-const rootReducer = combineReducers({ api: apiRequest, recommendations: recRequest });
+const rootReducer = combineReducers({
+  api: apiRequest,
+  recommendations: recRequest,
+  favorites: favoriteRecipes,
+});
 
 export default rootReducer;
