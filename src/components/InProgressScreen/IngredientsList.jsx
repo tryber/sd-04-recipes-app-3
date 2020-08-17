23edfca5c3b6dc0e85ef8c9af2fdfd75d9aa86e7
djonata-dias <diasdjonata@gmail.com>
2020-08-16 21:15:53 -0300

    item de receita em progresso riscando quando selecionada

--- src/components/InProgressScreen/IngredientsList.jsx
+++ src/components/InProgressScreen/IngredientsList.jsx
@@ -2,19 +2,38 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const IngredientsList = ({ recipeInfo, recipe, isFood }) => (
-  <div>
+  <form onChange={(e) => console.log(e.target)}>
     {recipeInfo(recipe[0], isFood).ingredients.map((item, index) => (
       <li
         key={`${item.ingredient}${item.measure}`}
         data-testid={`${index}-ingredient-step`}
+        style={{ listStyle: 'none' }}
       >
-        <input type="checkbox" name={item.ingredient} id={item.ingredient} />
-        <label htmlFor={item.ingredient}>
-          {`${item.ingredient} - ${item.measure}`}
+        <label htmlFor={`ingredient${index}`}>
+          <input
+            type="checkbox"
+            name="ingredient-item"
+            id={`ingredient${index}`}
+            className="ingredient-item"
+            onClick={(e) => {
+              const itemList = document.getElementById(item.ingredient);
+              console.log(itemList);
+              if (e.target.checked) {
+                itemList.style.textDecoration = 'line-through';
+              } else {
+                itemList.style.textDecoration = 'initial';
+              }
+            }}
+          />
+          <span id={item.ingredient}>
+            {item.ingredient}
+            {item.measure ? ' - ' : ' '}
+            {item.measure}
+          </span>
         </label>
       </li>
     ))}
-  </div>
+  </form>
 );
 
 IngredientsList.propTypes = {
