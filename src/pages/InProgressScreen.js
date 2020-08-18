ce3ab6363dbd8ed148a7d43e6ffa63fbac7b2181
djonata-dias <diasdjonata@gmail.com>
2020-08-17 21:49:47 -0300

    ajuste de bug e de CC

--- src/pages/InProgressScreen.js
+++ src/pages/InProgressScreen.js
@@ -6,13 +6,8 @@ import { fetchMeals } from '../actions/apiRequest';
 import IngredientsList from '../components/InProgressScreen/IngredientsList';
 import ShareBtn from '../components/InProgressScreen/ShareBtn';
 import FavoriteBtn from '../components/DetailsScreen/FavoriteBtn';
+import { recipeKeysToArray } from '../helpers';
 // import { getLS } from '../helpers';
-
-// Get the desired object key from the recipe and returns an array
-const recipeKeysToArray = (recipe, key) => Object.keys(recipe)
-  .filter((item) => item.startsWith(key))
-  .map((item) => recipe[item])
-  .filter((item) => item !== '' && item !== null);
 
 // Returns an array of objects with ingredient/measure pairs
 const getIngredients = (recipe) => {
